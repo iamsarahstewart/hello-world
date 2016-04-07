2266ba7e172b71610cf95ed47a7430a09e3d965f
iamsarahstewart <iamsarahstewart@gmail.com>
2016-04-07 11:44:39 +0100

    Added files via upload

---
+++ javascript.js
@@ -0,0 +1,15 @@
+/* No Conflict with Moo Tools */
+
+jQuery.noConflict();
+
+jQuery(document).ready(function($){
+
+/*
+*
+* Add All Your Functions Here. Within scope of no conflict so use $ freely.
+*
+*/
+
+/* End */
+
+});
